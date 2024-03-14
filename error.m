b17502c7d3f9461d9c95429980dfb61900094112
Sam <copperboat@hey.com>
2024-03-13 22:46:56 -0500

    don't store error message in provided target if the target is not empty

--- error.m
+++ error.m
@@ -3,12 +3,13 @@
 
 #include "error.h"
 
-// Log an error to console and optionally set target to the error message.
+// Log an error to console and optionally set target to the error message if
+// target is not nil and doesn't already contain a message.
 void logError(const char **target, NSString *format, ...) {
   NSString *message = [NSString stringWithFormat:@"%@", format];
   NSLog(@"%@", message);
 
-  if (target != nil) {
+  if (target != nil && strlen(*target) == 0) {
     *target = [message UTF8String];
   }
 }
