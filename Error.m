e77d2998c0f1f3599523bf4c1e293c5e906452e2
Sam <copperboat@hey.com>
2024-03-20 14:06:12 -0500

    error: wrap error messages

--- Error.m
+++ Error.m
@@ -4,12 +4,16 @@
 #include "Error.h"
 
 // Log an error to console and optionally set target to the error message if
-// target is not nil and doesn't already contain a message.
+// target is not nil. If target already contains a message, it is appended to
+// the new message.
 void logError(const char **target, NSString *format, ...) {
   NSString *message = [NSString stringWithFormat:@"%@", format];
   NSLog(@"%@", message);
 
-  if (target != nil && strlen(*target) == 0) {
+  if (target != nil) {
+    if (strlen(*target) > 0) {
+      message = [NSString stringWithFormat:@"%@: %s", message, *target];
+    }
     *target = [message UTF8String];
   }
 }
